e7058a4b71212068e3423db947de205fe615b38c
hubby12a <paolauwase555@gmail.com>
2026-01-28 13:00:23 +0200

    TSX errors are back in the play

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "PaolaUwase",
   description: "Paola-Ready to transform your vision into a high-performance digital reality!",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
